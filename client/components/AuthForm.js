63cb8b918e2fe04eb9300c1009b7c3c0b8ceec42
Tandeep Gill <gill.tandeep@gmail.com>
2021-12-14 21:57:44 +0000

    Sigin/Signup functionality with forms. Additional minor changes.

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { authenticate } from '../store';
 
@@ -9,26 +10,79 @@ const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div className='inline-block'>
-          <label htmlFor='username'>
-            <small>Username</small>
-          </label>
-          <input name='username' type='text' />
+    <>
+      <div className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-700'>
+            Sign in to your account
+          </h2>
+          <p className='mt-2 text-center text-sm text-gray-600'>
+            Or if you don't have an account{' '}
+            <Link
+              to='/signup'
+              className='font-medium text-orange-600 hover:text-orange-700'
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
-        <div className='inline-block'>
-          <label htmlFor='password'>
-            <small>Password</small>
-          </label>
-          <input name='password' type='password' />
+
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+          <div className='bg-orange-50 py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+            <form className='space-y-6' onSubmit={handleSubmit} name={name}>
+              <div>
+                <label
+                  htmlFor='username'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Username
+                </label>
+                <div className='mt-1'>
+                  <input
+                    name='username'
+                    type='text'
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm'
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor='password'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Password
+                </label>
+                <div className='mt-1'>
+                  <input
+                    name='password'
+                    type='password'
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm'
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type='submit'
+                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
+                >
+                  {displayName}
+                </button>
+                {error && error.response && (
+                  <div className='text-slate-700 mt-3'>
+                    {' '}
+                    {error.response.data}{' '}
+                  </div>
+                )}
+              </div>
+            </form>
+          </div>
         </div>
-        <div className='inline-block'>
-          <button type='submit'>{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
@@ -42,7 +96,7 @@ const AuthForm = (props) => {
 const mapLogin = (state) => {
   return {
     name: 'login',
-    displayName: 'Login',
+    displayName: 'Sign in',
     error: state.auth.error,
   };
 };
@@ -50,7 +104,7 @@ const mapLogin = (state) => {
 const mapSignup = (state) => {
   return {
     name: 'signup',
-    displayName: 'Sign Up',
+    displayName: 'Sign up',
     error: state.auth.error,
   };
 };
--- client/components/Footer.js
+++ client/components/Footer.js
@@ -53,7 +53,7 @@ const navigation = [
 
 const Footer = () => {
   return (
-    <footer className='bg-white border-t border-gray-200'>
+    <footer className='bg-white border-t border-gray-200 fixed bottom-0 w-full'>
       <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8'>
         <div className='flex justify-center space-x-6 md:order-2'>
           {navigation.map((item) => (
