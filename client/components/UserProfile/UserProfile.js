ce98ed1626755bfc3530160219a11a93cccfcd8d
Tandeep Gill <gill.tandeep@gmail.com>
2022-03-17 22:10:08 -0700

    User profile styling mobile responsive.

--- client/components/UserProfile/UserProfile.js
+++ client/components/UserProfile/UserProfile.js
@@ -57,59 +57,65 @@ class UserProfile extends React.Component {
     };
 
     return (
-      <div>
-        <div className='max-w-2xl mx-auto mt-12 mb-4 px-4 sm:px-6 lg:max-w-7xl lg:px-8 min-h-screen'>
-          <div className='flex items-center mb-4'>
-            <h1 className='text-2xl font-bold underline text-orange-600'>
+      <div className='min-h-screen w-full'>
+        <div className='flex w-full mx-auto mt-12 mb-4 px-4 sm:px-6 lg:px-8'>
+          <div className='flex flex-col  mx-auto items-center mb-4 justify-center w-full'>
+            <h1 className='text-2xl font-bold underline text-orange-600 mb-4'>
               USER PROFILE
             </h1>
-          </div>
-          <div className='flex flex-col flex-wrap'>
-            <div className='flex mb-6 items-center bg-orange-100 border-2 border-orange-600 rounded-md w-2/5'>
-              <img
-                src={user.imageURL}
-                alt='Profile picture.'
-                className='w-48 rounded-md m-3'
-              />
-              <div className='m-4'>
-                <h2 className='py-2'>
-                  <span className='font-semibold'>First Name:</span>{' '}
-                  {user.firstName}
-                </h2>
-                <h2 className='py-2'>
-                  {' '}
-                  <span className='font-semibold'>Last Name:</span>{' '}
-                  {user.lastName}
-                </h2>
-                <h2 className='py-2'>
-                  {' '}
-                  <span className='font-semibold'>Username:</span>{' '}
-                  {user.username}
-                </h2>
-                <h2 className='py-2'>
-                  {' '}
-                  <span className='font-semibold'>Member Since:</span>{' '}
-                  {dateFormat(user.createdAt)}
-                </h2>
+            <div className='flex flex-col flex-wrap bg-orange-100 border-2 border-orange-600 rounded-md w-2/3 lg:w-1/3'>
+              <div className='flex flex-col lg:flex-row items-center justify-center'>
+                <img
+                  src={user.imageURL}
+                  alt='Profile picture.'
+                  className='w-52 rounded-md m-2'
+                />
+                <div className='m-4'>
+                  <h2 className='py-2'>
+                    <span className='font-semibold'>First Name:</span>{' '}
+                    {user.firstName}
+                  </h2>
+                  <h2 className='py-2'>
+                    {' '}
+                    <span className='font-semibold'>Last Name:</span>{' '}
+                    {user.lastName}
+                  </h2>
+                  <h2 className='py-2'>
+                    {' '}
+                    <span className='font-semibold'>Username:</span>{' '}
+                    {user.username}
+                  </h2>
+                  <h2 className='py-2'>
+                    {' '}
+                    <span className='font-semibold'>Member Since:</span>{' '}
+                    {dateFormat(user.createdAt)}
+                  </h2>
+                </div>
               </div>
             </div>
 
-            <div className='flex justify-center items-center'>
-              <div className='w-2/5 h-60 bg-orange-600 text-white font-semibold rounded-md p-4 mr-5'>
-                <h2 className='text-3xl text-center mb-5'>TOTAL PROPERTIES</h2>
-                <p className='text-9xl text-center'>
+            <div className='flex flex-col lg:flex-row justify-center items-center w-full lg:w-2/3'>
+              <div className='flex flex-col justify-center items-center w-2/3 lg:1/5 h-40 lg:h-60 bg-orange-300 text-white font-semibold rounded-md p-4 lg:mx-4 my-3'>
+                <h2 className='text-2xl lg:text-4xl text-center mb-5'>
+                  PROPERTIES
+                </h2>
+                <p className='text-6xl lg:text-9xl text-center'>
                   {propertyCount(properties)}
                 </p>
               </div>
-              <div className='w-2/5 h-60 bg-orange-600 text-white font-semibold rounded-md p-4 mx-8'>
-                <h2 className='text-3xl text-center mb-5'>CURRENT TENANTS</h2>
-                <p className='text-9xl text-center'>
+              <div className='flex flex-col justify-center items-center w-2/3 lg:1/5 h-40 lg:h-60 bg-orange-300 text-white font-semibold rounded-md p-4 lg:mx-4 my-3'>
+                <h2 className='text-2xl lg:text-4xl text-center mb-5'>
+                  OCCUPIED
+                </h2>
+                <p className='text-6xl lg:text-9xl text-center'>
                   {currentTenantCount(tenants)}
                 </p>
               </div>
-              <div className='w-2/5 h-60 bg-orange-600 text-white font-semibold rounded-md p-4 ml-5'>
-                <h2 className='text-3xl text-center mb-5'>PREVIOUS TENANTS</h2>
-                <p className='text-9xl text-center'>
+              <div className='flex flex-col justify-center items-center w-2/3 lg:1/5 h-40 lg:h-60 bg-orange-300 text-white font-semibold rounded-md p-4 lg:mx-4 my-3'>
+                <h2 className='text-2xl lg:text-4xl text-center mb-5'>
+                  VACANT
+                </h2>
+                <p className='text-6xl lg:text-9xl text-center'>
                   {previousTenantCount(tenants)}
                 </p>
               </div>
