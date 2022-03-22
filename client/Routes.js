0b8bfc73f2427a887cf6f445c95db2477478e6bd
Tandeep Gill <gill.tandeep@gmail.com>
2022-03-22 16:08:12 -0700

    Home import from Routes removed, no longer throwing error.

--- client/Routes.js
+++ client/Routes.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
-import Home from './components/Home/Home';
 import AllProperties from './components/AllProperties/AllProperties';
 import SingleProperty from './components/SingleProperty/SingleProperty';
 import AllTenants from './components/AllTenants/AllTenants';
